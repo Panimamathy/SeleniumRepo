d2e8bb441eff5a7e7e8ac70b3c32ee9ae44c6509
Panimamathy <97274000+Panimamathy@users.noreply.github.com>
2025-11-08 18:35:49 +0530

    Add files via upload
    HomeAssignmentDone

---
+++ Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version = 91.0f;
+		System.out.println("version of type float with a value of" +" " +version);
+		String developer = "Google";
+		System.out.println("developer of type String with a value of" +" "+developer);
+		boolean beta = false;
+		System.out.println("isBeta of type booleanwith a value of" +" " +beta);
+		int releaseYear = 2008;
+		System.out.println("releaseYear of type int with a value of" +" "+releaseYear);
+		char shortcut = 'C';
+		System.out.println("shortcutKey of type charwith a value of" +" " +shortcut);
+		
+	}
+
+}
